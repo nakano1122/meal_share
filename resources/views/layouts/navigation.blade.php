ac4c72cb5b138e53f60bcfc7dc3e214b6f71b2cb
GitHubのユーザ名 <GitHub>
2023-12-30 20:59:01 +0900

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,7 +82,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('チュートリアル') }}
+                {{ __('使い方') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('index')" :active="request()->routeIs('index')">
                 {{ __('ホーム') }}
